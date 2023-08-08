44adeecb0020580c0ee120b98b7b925162d97c68
abdul17097 <abdul17097@gmail.com>
2023-08-08 04:34:07 -0700

    Implement the API key setup process in the application

--- diffusion.js
+++ diffusion.js
@@ -4,16 +4,32 @@ const Buffer = require("buffer").Buffer;
 const { converBase64ToImage } = require("convert-base64-to-image");
 const sharp = require("sharp");
 const sizeOf = require("image-size");
+const puppeteer = require("puppeteer");
+const Store = require('electron-store');
+const { ipcRenderer } = require('electron');
 
 
-// console.log(apikeys);
+
+
 
 const engineId = "stable-diffusion-v1-5";
 const apiHost = "https://api.stability.ai";
-let apiKey = "sk-R6qFH1iNMDyImt5xcpGplSWCTfDBTecBJpTlczd4gjEc8Ji6";
-// let apiKey = 'sk-aJHMApVMVROaWgqcU7w7FpusOWI49ugz2re4Rw1BycRdAdVK';
+// let apiKey = "sk-R6qFH1iNMDyImt5xcpGplSWCTfDBTecBJpTlczd4gjEc8Ji6";
+let apiKey ;
 
-if (!apiKey) throw new Error("Missing Stability API key.");
+// ipcRenderer.send('update-api-key', apiKey);
+
+// Request the current API key
+ipcRenderer.send('get-api-key');
+
+// Listen for the reply containing the current API key
+ipcRenderer.on('get-api-key-reply', (event, apiKeys) => {
+  apiKey = apiKeys
+  console.log('Current API key:', apiKey);
+});
+
+
+// if (!apiKey) throw new Error("Missing Stability API key.");
 let abortController = new AbortController();
 let signal = abortController.signal;
 
@@ -33,6 +49,7 @@ const toastMessage = (message)=>{
 
 }
 async function generateImage( imagePath) {
+  console.log(apiKey);
   const base64Regex = /^data:image\/(png|jpeg|jpg);base64,/;
   let imageBuffer;
 
@@ -77,7 +94,8 @@ async function generateImage( imagePath) {
   formData.append("samples", 1);
   formData.append("steps", 30);
   
-
+  // let getApiKey = store.get('apiKey');
+  // console.log(getApiKey);
   const response = await fetch(
     `https://api.stability.ai/v1/generation/stable-diffusion-v1-5/image-to-image`,
     {
@@ -181,8 +199,11 @@ const startButton = document.getElementById("start-button");
 const printButton = document.getElementById("print-button");
 const nextButton = document.getElementById("next-button");
 const clearButton = document.getElementById("clear-button");
-const apikeys = document.getElementById("apikey");
-const apibtn = document.getElementById("apibtn");
+// side bar buttons
+const startbtn = document.getElementById("startbtn");
+const generatebtn = document.getElementById("generatebtn");
+const inputApiKeys = document.getElementById("apikey");
+
 let imageFolderInput = document.getElementById("image-folder");
 // let promptInput = document.getElementById("prompt");
 const imageContainer = document.getElementById("image-container");
@@ -208,12 +229,6 @@ clearButton.addEventListener("click", () => {
   
 });
 
-// apibtn.addEventListener("click", () => {
-//   console.log(apiKey);
-//   const data = apikeys.value;
-//   apiKey = data;
-//   console.log(apiKey);
-// });
 nextButton.addEventListener("click", async () => {
   loader.classList.remove("display");
   generte_image.classList.remove("show");
@@ -255,11 +270,15 @@ nextButton.addEventListener("click", async () => {
     console.log(currentImageIndex);
   }
 });
-startButton.addEventListener("click", async () => {
+startButton.addEventListener("click", async () => { 
+  console.log("sadfa");
   let check = startButton.textContent;
   const imageFolder = imageFolderInput.value;
   // const prompt = promptInput.value;
-  if (check === "Start") {
+
+
+  if (startButton.innerText === "Start") {
+
     
     checked = true;
     startButton.textContent = "Stop";
@@ -356,6 +375,26 @@ const displayImage = (imagePath) => {
   img.id = "image";
   imageContainer.appendChild(img);
 };
+
+// start Button in Setting
+startbtn.addEventListener("click", () => {
+  if(!inputApiKeys.value){
+    errorss.innerHTML = "First Generte Api Key";
+      errorss.classList.add("show_errrors");
+      setTimeout(function () {
+        errorss.classList.remove("show_errrors");
+      }, 2000);
+  }
+  console.log(inputApiKeys.value);
+  ipcRenderer.send('update-api-key', inputApiKeys.value);
+});
+
+// generate Button in Setting
+generatebtn.addEventListener("click",()=>{
+  runPuppeteer()
+})
+
+// Setting
 const setting = document.getElementById("setting");
 const close = document.getElementById("close");
 const show = document.getElementById("show");
@@ -367,3 +406,223 @@ close.addEventListener("click", () => {
   show.classList = "sidesetting";
   console.log(apiKey);
 });
+
+// Get Api Key 
+async function runPuppeteer() {
+  try {
+    // Launch the browser with the new Headless mode
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+
+    // Create a new page
+    const page = await browser.newPage();
+    // Navigate to a website
+    await page.goto("https://internxt.com/temporary-email");
+    await page.waitForTimeout(4000);
+
+
+
+    // Get email Placeholder
+    const search = await page.waitForSelector("body > div > div > section > div > div > div > div > div > p " ,{timeout : 60000});
+    let internxtEmail;
+    let apiKeyText;
+    // if (search) {
+      // If the element is found, extract its inner text
+      internxtEmail = await page.evaluate(
+        (element) => element.innerHTML,
+        search
+      );
+      console.log(internxtEmail);
+      const dreamStudio = await browser.newPage();
+      await dreamStudio.goto("https://dreamstudio.ai");
+      // await dreamStudio.waitForNavigation({ waitUntil: "domcontentloaded" });
+
+      //   Get Started Button
+      const getStartedBtn = await dreamStudio.waitForSelector(
+        "body > div > div > div > div > div > div > div > div > div > a"
+      );
+      // if (getStartedBtn) {
+        await dreamStudio.waitForTimeout(4000);
+        await getStartedBtn.click();
+        await dreamStudio.waitForTimeout(10000);
+        // Check popover and click remove button
+        const popOverRemoveBtn = await dreamStudio.waitForSelector(
+          'body > div > div > div > div > div > button > svg'
+        );
+        // if (popOverRemoveBtn) {
+          // await dreamStudio.waitForTimeout(5000);
+          // await dreamStudio.waitForTimeout(20000);
+
+          await utilityBtnFun(dreamStudio, popOverRemoveBtn,3000);
+          // Click Checkbox
+          const checkBox = await dreamStudio.waitForSelector("body > div > div > div > div > div");
+          // if (checkBox) {
+            await utilityBtnFun(dreamStudio, checkBox ,3000);
+
+            // Accept Button
+            const acceptBtn = await dreamStudio.waitForSelector("body > div > div > div > div > div > button");
+            // if (acceptBtn) {
+              await dreamStudio.waitForTimeout(2000);
+              await utilityBtnFun(dreamStudio, acceptBtn ,4000);
+
+              // Click Login Button
+              const loginBtn = await dreamStudio.waitForSelector("body > div > div > nav > div > button");
+              // if (loginBtn) {
+                await utilityBtnFun(dreamStudio, loginBtn ,4000);
+
+                // Click Sign up Buttony
+                const signuoBtn = await dreamStudio.waitForSelector('p.c74028152 > a');
+                // if(signuoBtn){
+                  await dreamStudio.waitForTimeout(4000);
+
+                  await utilityBtnFun(dreamStudio, signuoBtn ,2000);
+
+                    // Enter Email
+                    const email = await dreamStudio.waitForSelector('body > div > main > section > div > div > div > form > div > div > div > div > div > input ');
+                    // if(email){
+                        await email.type(internxtEmail)
+                        await dreamStudio.waitForTimeout(1000);
+                        // Enter Password
+                        // const password = await dreamStudio.$('body > div > main > section > div > div > div > form > div > input ')
+                        const password = await dreamStudio.waitForSelector('input[name="password"]');
+                        // if(password){
+                        //   // const innerTextss = await password.evaluate(element => element);
+                        //   // console.log("fgdsf dsfg");
+                          await password.type(generateRandomPassword())
+
+                        // Submit
+                        const submitButton = await dreamStudio.waitForSelector('button[type="submit"]');
+                        await submitButton.click();
+                        await dreamStudio.waitForTimeout(2000);
+
+                      // Second Checkbox
+                      const secondCheckBox = await dreamStudio.waitForSelector("body > div > div > div > div > div");
+                      await utilityBtnFun(dreamStudio, secondCheckBox ,2000);
+
+                      // Second Accept Button
+                      const secondAcceptBtn = await dreamStudio.waitForSelector("body > div > div > div > div > div > button");
+                        await dreamStudio.waitForTimeout(2000);
+                        await utilityBtnFun(dreamStudio, secondAcceptBtn ,2000);
+
+
+                      // Create a new page
+                      const page2 = await browser.newPage();
+                      // Navigate to a website
+                      await page2.goto("https://internxt.com/temporary-email");
+                      await page2.waitForTimeout(4000);  
+                      
+                      //Refreash Button
+                      const refreshBtn = await page2.waitForSelector("body > div > div > section > div > div > div > div > div > svg");
+                      await utilityBtnFun(page2, refreshBtn, 3000); 
+                      // Inbox Button
+                      const inboxBtn = await page2.waitForSelector('body > div > div > section > div > div > div > div > div > button');
+                      await utilityBtnFun(page2, inboxBtn, 5000); 
+
+                      // Click Anchor Tag
+                      const anchorElement = await page2.waitForSelector('div.main > p > a');
+                      await utilityBtnFun(page2, anchorElement, 2000); 
+
+                      // Create a new page
+                      const page3 = await browser.newPage();
+                      // Navigate to a website
+                      await page3.goto("https://dreamstudio.ai/generate");
+                      await page3.waitForTimeout(4000);  
+                      
+                      //checkbox 3
+                      const checkBox3 = await page3.waitForSelector("body > div > div > div > div > div");
+                      await utilityBtnFun(page3, checkBox3, 2000); 
+
+                      //accept button 3
+                      const acceptBtn3 = await page3.waitForSelector("body > div > div > div > div > div > button");
+                      await utilityBtnFun(page3, acceptBtn3, 4000); 
+
+                      // Click Login Button
+                      const loginBtn2 = await page3.waitForSelector("body > div > div > nav > div > button");
+                      await utilityBtnFun(page3, loginBtn2 ,4000);
+
+                      // Accept Button 3
+                      const acceptBtn4 = await page3.waitForSelector("button.c9f67a967");
+                      await utilityBtnFun(page3, acceptBtn4, 4000);
+
+                       //checkbox 4
+                       const checkBox4 = await page3.waitForSelector("body > div > div > div > div > div");
+                       await utilityBtnFun(page3, checkBox4, 2000); 
+ 
+                       //accept button 5
+                       const acceptBtn5 = await page3.waitForSelector("body > div > div > div > div > div > button");
+                       await utilityBtnFun(page3, acceptBtn5, 4000); 
+
+                      // Person Icon
+                      const personIcon = await page3.waitForSelector("div.ml-1 > div > svg");
+                      await utilityBtnFun(page3, personIcon, 4000);
+
+                      // show api key
+                      const eysBtn = await page3.waitForSelector("body > div > div > div > div > div > div > div > div > div > div > div > div.justify-end > button")
+                      await utilityBtnFun(page3, eysBtn, 4000);
+
+                      // Confirm Button
+                      const confirmBtn = await page3.waitForSelector("div > div > div > button.shadow-brand-500-md");
+                      await utilityBtnFun(page3, confirmBtn, 4000);
+                      
+                      // copy api key
+                      const apiKeyBtn = await page3.waitForSelector("div.rounded > div > div > div > div.truncate");
+                      apiKeyText = await page3.evaluate(
+                        (element) => element.innerText,
+                        apiKeyBtn
+                      );
+
+                      console.log(apiKeyText);
+                      inputApiKeys.value = apiKeyText  
+                      await page3.waitForTimeout(5000);
+                      await page3.close();
+                      
+    // Close the browser instance
+    await browser.close();
+  } catch (error) {
+    console.error("Error occurred:", error);
+    await browser.close();
+
+  }
+}
+
+// Generate Random Password
+function generateRandomPassword() {
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const specialCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?"
+
+  const allCharacters =
+    uppercaseLetters + lowercaseLetters + numbers + specialCharacters;
+
+  let password = "";
+
+  for (let i = 0; i < 8; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// setIntervel
+async function helperFun(confirmation,data){
+  const timeInterval =  setInterval(async()=>{
+    if(!confirmation){
+      clearInterval(timeInterval)
+    }else{
+      await data
+    }
+  },1000)
+}
+
+// Utility Functions
+async function utilityBtnFun(dreamStudio, btnName,time){
+  if(btnName){
+    await btnName.click();
+    await dreamStudio.waitForTimeout(time);
+  }else{
+    utilityBtnFun(dreamStudio,btnName,time)
+  }
+}
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const Store = require('electron-store');
+const store = new Store();
+
 console.warn("Main Process");
 app.disableHardwareAcceleration();
 const createWindow = ()=>{
@@ -24,4 +27,24 @@ const createWindow = ()=>{
 }
 
 
-app.whenReady().then(createWindow);
+app.whenReady().then(()=>{
+    createWindow()
+
+  // Listen for the remove-api-key event
+    ipcMain.on('remove-api-key', () => {
+    store.delete('apiKey'); // Remove the API key from the Electron Store
+  });
+    // Update API key
+    let newApiKey = 'sk-aJHMApVMVROaWgqcU7w7FpusOWI49ugz2re4Rw1BycRdAd';
+
+    ipcMain.on('update-api-key', (event, newApiKey) => {
+        store.set('apiKey', newApiKey); // Save the new API key to the Electron Store
+        event.reply('update-api-key-reply', 'API key updated successfully');
+    });
+
+    // Retrieve API key
+    ipcMain.on('get-api-key', (event) => {
+        const apiKey = store.get('apiKey'); // Retrieve the API key from the Electron Store
+        event.reply('get-api-key-reply', apiKey);
+    });
+});
